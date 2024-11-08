de287b2fbd38b515fedd14b8f26a54702ec48fc9
Way <way11229@gmail.com>
2024-11-08 11:09:32 +0800

    finished day1-2

---
+++ day1-2/show_reverse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Description: Print reverse string 
+#Write by Way(way11229@gmail.com)
+#Version: v1.0.0
+
+read  -p  "Please enter a string:  " inputStr 
+
+echo $inputStr | rev
+
+exit 0
